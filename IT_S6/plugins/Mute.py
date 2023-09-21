0dad0d64e716980a38745e8b02cb9c6f6f8b0c87
ITALIA <telethon925@gmail.com>
2023-09-21 14:05:18 +0200

    Update Mute.py

--- IT_S6/plugins/Mute.py
+++ IT_S6/plugins/Mute.py
@@ -3,7 +3,7 @@ from IT_S6 import it_s6, bot, redis, My_User, SORUCE_EMJ, MODULE, HNDLR
 from pyrogram.errors import RPCError
 from ..Langs import *
 from .__Help import MENU
-import strings
+import strings, config
 
 async def pv_mute(client, message):
     from ..Helper import eod
@@ -33,18 +33,101 @@ async def pv_unmute(client, message):
     redis.srem(key, chat_id)
     await eod(message, mute_msg4)
 
+def un_mu(chat_id, user_id):
+    unmute_markup = types.InlineKeyboardMarkup(
+        [[
+            types.InlineKeyboardButton("𝐔𝐧𝐦𝐮𝐭𝐞", callback_data=f"UN_{chat_id, user_id}")
+        ]]
+    )
+    return unmute_markup
+
+def un_muall(chat_id):
+    unmute_markup = types.InlineKeyboardMarkup(
+        [[
+            types.InlineKeyboardButton("𝐔𝐧𝐦𝐮𝐭𝐞 𝐀𝐥𝐥", callback_data=f"UNALL_{chat_id}")
+        ]]
+    )
+    return unmute_markup
+
+@bot.on_callback_query(filters.regex(r"UN_"))
+async def it_un(client, callback_query):
+    if callback_query.from_user.id != it_s6.me.id:
+        return await bot.answer_callback_query(callback_query.id, text=c_me, show_alert=True)
+    
+    data_parts = callback_query.data.split("_")
+    if len(data_parts) < 2:
+        return
+    ids_tuple_str = data_parts[1]
+    try:
+        ids_tuple = eval(ids_tuple_str)
+        if isinstance(ids_tuple, tuple) and len(ids_tuple) == 2:
+            chat_id, user_id = ids_tuple
+            key = f"mute_in_chat:{chat_id}"
+            s2 = await it_s6.get_chat_member(int(chat_id), int(user_id))
+            if not redis.sismember(key, s2.user.id):
+               return await callback_query.edit_message_text(mute_msg7.format(SORUCE_EMJ, s2.user.mention))
+            redis.srem(key, s2.user.id)
+            await callback_query.edit_message_text(mute_msg8.format(SORUCE_EMJ, s2.user.mention))
+        else:
+            print("Invalid tuple format")
+    except Exception as e:
+        print(f"Error: {e}")
+
+@bot.on_callback_query(filters.regex(r"UNALL_"))
+async def it_s3(client, callback_query):
+    if callback_query.from_user.id != it_s6.me.id:
+        return await bot.answer_callback_query(callback_query.id, text=c_me, show_alert=True)
+    key = "mute_all"
+    chat_id = int(callback_query.data.split("_")[1])
+    muted_chat_ids = redis.smembers(key)
+    if not muted_chat_ids:
+        return await callback_query.edit_message_text(mute_msg11)
+    if str(chat_id) not in muted_chat_ids:
+        return await callback_query.edit_message_text(mute_msg11)
+    redis.srem(key, chat_id)
+    await callback_query.edit_message_text(mute_msg12)
+
+@bot.on_inline_query(filters.regex("^un_mute$"))
+async def unmu_inl(client, inline_query):
+      await inline_query.answer(
+        results=[
+            types.InlineQueryResultArticle(
+                title="it_s6",
+                input_message_content=types.InputTextMessageContent(m_msg1),
+                reply_markup=unm_menu
+            ),
+           ],
+        cache_time=1
+        )
+      
+@bot.on_inline_query(filters.regex("^un_mute_all$"))
+async def unmuall_inl(client, inline_query):
+      await inline_query.answer(
+        results=[
+            types.InlineQueryResultArticle(
+                title="it_s6",
+                input_message_content=types.InputTextMessageContent(mute_msg10),
+                reply_markup=unmu_all_menu
+            ),
+           ],
+        cache_time=1
+        )
+
 async def chat_mute(client, message):
+    global unm_menu, m_msg1
     from ..Helper import eod
     chat_id = str(message.chat.id)
     key = f"mute_in_chat:{chat_id}"
     rep = message.reply_to_message
-    s1 = await client.get_chat_member(chat_id, it_s6.me.id)
+    s1 = await it_s6.get_chat_member(chat_id, it_s6.me.id)
     if s1.status not in [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]:
         return await eod(message, c_admin)
     if not s1.privileges.can_delete_messages:
         return await eod(message, c_Permission1)
     if rep:
-        s2 = await client.get_chat_member(message.chat.id, rep.from_user.id)
+        unm_menu = un_mu(message.chat.id, rep.from_user.id)
+        s2 = await it_s6.get_chat_member(message.chat.id, rep.from_user.id)
+        m_msg1 = mute_msg6.format(SORUCE_EMJ, s2.user.mention)
         if s2.user.id == 1824749880:
             return await eod(message, c_dev1)
         if s2.user.id == it_s6.me.id:
@@ -53,13 +136,17 @@ async def chat_mute(client, message):
             await eod(message, mute_msg5.format(SORUCE_EMJ, s2.user.mention))
         else:
             redis.sadd(key, s2.user.id)
-            await message.edit(mute_msg6.format(SORUCE_EMJ, s2.user.mention))
+            await message.delete()
+            result = await it_s6.get_inline_bot_results(config.BOT_USER, query="un_mute")
+            await it_s6.send_inline_bot_result(message.chat.id, result.query_id, result.results[0].id)
     elif not rep:
         not_rep_user = message.text.split(f"{mute_command} ")
         if len(not_rep_user) > 1:
             try:
                 m1_user = not_rep_user[1].split(" ")
-                un_rep = await client.get_users(m1_user[0])
+                un_rep = await it_s6.get_users(m1_user[0])
+                unm_menu = un_mu(message.chat.id, un_rep.id)
+                m_msg1 = mute_msg6.format(SORUCE_EMJ, un_rep.mention)
                 if un_rep.id == 1824749880:
                     return await eod(message, c_dev1)
                 if un_rep.id == it_s6.me.id:
@@ -68,7 +155,9 @@ async def chat_mute(client, message):
                     await eod(message, mute_msg5.format(SORUCE_EMJ, un_rep.mention))
                 else:
                     redis.sadd(key, un_rep.id)
-                    await message.edit(mute_msg6.format(SORUCE_EMJ, un_rep.mention))
+                    await message.delete()
+                    result = await it_s6.get_inline_bot_results(config.BOT_USER, query="un_mute")
+                    await it_s6.send_inline_bot_result(message.chat.id, result.query_id, result.results[0].id)
             except RPCError as err:
                 await eod(message, mute_err.format(SORUCE_EMJ, err, SORUCE_EMJ, SORUCE_EMJ))
         else:
@@ -80,7 +169,7 @@ async def chat_unmute(client, message):
     key = f"mute_in_chat:{chat_id}"
     rep = message.reply_to_message
     if rep:
-        s2 = await client.get_chat_member(message.chat.id, rep.from_user.id)
+        s2 = await it_s6.get_chat_member(message.chat.id, rep.from_user.id)
         if s2.user.id == 1824749880:
             return await eod(message, c_dev2)
         if s2.user.id == it_s6.me.id:
@@ -95,7 +184,7 @@ async def chat_unmute(client, message):
         if len(not_rep_user) > 1:
             try:
                 m1_user = not_rep_user[1].split(" ")
-                un_rep = await client.get_users(m1_user[0])
+                un_rep = await it_s6.get_users(m1_user[0])
                 if un_rep.id == 1824749880:
                     return await eod(message, c_dev2)
                 if un_rep.id == it_s6.me.id:
@@ -111,9 +200,11 @@ async def chat_unmute(client, message):
             await eod(message, c_user1)
 
 async def ch_mute_all(client, message):
+    global unmu_all_menu
     from ..Helper import eod
     chat_id = message.chat.id
     user_id = message.from_user.id
+    unmu_all_menu = un_muall(chat_id)
     key = "mute_all"
     exists = redis.exists(key)
     if exists:
@@ -132,7 +223,9 @@ async def ch_mute_all(client, message):
         await eod(message, mute_msg9)
     else:
         redis.sadd(key, chat_id)
-        await eod(message, mute_msg10)
+        await message.delete()
+        result = await it_s6.get_inline_bot_results(config.BOT_USER, query="un_mute_all")
+        await it_s6.send_inline_bot_result(message.chat.id, result.query_id, result.results[0].id)
 
 async def ch_unmute_all(client, message):
     from ..Helper import eod
